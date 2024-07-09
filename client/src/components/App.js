cf8f87f1f8b4393e35ac7cb6193ac4871077677c
briimiin <kipkorirbrimin005@gmail.com>
2024-07-09 15:00:51 +0300

    creates a login page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import RegisterForm from './RegisterForm';
+import LoginForm from './LoginForm';
 
 function App() {
-  return <h1>Project Client</h1>;
-}
+  return (
+    <div>
+      <h1>Art Gallery</h1>
+      <RegisterForm />
+      <LoginForm />
+    </div>
+  );}
 
 export default App;
