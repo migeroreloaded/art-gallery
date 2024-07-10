e295f1ba03ad59bee3307ab25b0b7f88c74d5a32
Migero <mikeroche138@gmail.com>
2024-07-10 22:03:56 +0300

    render Navbar.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
-import App from "./App";
- import "./index.css";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
