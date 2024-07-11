378b5be42f731d8a6488dffab6c4ddd35729c8e1
paulsaitabau <paulsaitabau05@gmail.com>
2024-07-11 10:03:24 +0300

    Load artists

--- client/src/components/Artist.js
+++ client/src/components/Artist.js
@@ -10,3 +10,24 @@ import {
     ArtistLoading,
     ErrorMessage
 } from './styles';
+
+const Artist = () => {
+    const [artists, setArtists] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchArtists = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:5555/artists');
+                setArtists(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setError('Error fetching artists. Please try again later.');
+                setLoading(false);
+            }
+        };
+
+        fetchArtists();
+    }, []);
