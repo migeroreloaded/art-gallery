8ac05309f9de0a376e3eb7e25dc947c841ec3083
Elvis Moses Tangasaiti <elvis.moses@student.moringaschool.com>
2024-07-17 15:17:24 +0300

    improves welcomepage styling

--- client/src/components/styles.js
+++ client/src/components/styles.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
- background-color: #fff;
- border-radius: 10px;
- box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
- position: relative;
- overflow: hidden;
- width: 1000px;
- height: 700px;
- max-width: 100%;
- min-height: 400px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  display: flex;
+  overflow: hidden;
 `;
 
 export const SignUpContainer = styled.div`
